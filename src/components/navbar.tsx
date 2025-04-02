528f134dc55670b49012d59457d209ba0f3d8efe
peperoni <brucewayne0002@proton.me>
2025-04-03 00:24:49 +0100

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { PhoneCall } from "lucide-react"
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 export default function Navbar() {
   const [isVisible, setIsVisible] = useState(false);
@@ -21,13 +24,13 @@ export default function Navbar() {
         : "opacity-0 pointer-events-none"
         }`}
     >
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
-        <h1 className="text-2xl font-bold text-white">My Portfolio</h1>
-        <ul className="flex space-x-6">
-          <li><a href="#about" className="text-white">About</a></li>
-          <li><a href="#projects" className="text-white">Projects</a></li>
-          <li><a href="#contact" className="text-white">Contact</a></li>
-        </ul>
+
+      <div className="max-w-5xl mx-auto flex items-center justify-between">
+        <h1 className="text-2xl font-bold text-white">Dylan Byrne</h1>
+        <Button variant={"outline"} className="ml-auto">
+          <PhoneCall />
+          <Link href="/login">Ready to Book a Call?</Link>
+        </Button>
       </div>
     </nav>
   );
