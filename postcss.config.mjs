1329af5084f867a044d51e48a7b28f0413fa2266
peperoni <brucewayne0002@proton.me>
2025-04-04 16:17:24 +0100

    refactoring

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,13 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss').Config} */
+const config = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
 };
 
+export default config;
 
